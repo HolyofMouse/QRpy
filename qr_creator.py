7a3ad6792437ab4d55c2166b0f79315703235103
Vova <vladimirresumeforwork@gmail.com>
2025-08-11 09:06:24 +0300

    Add function create_word_in_qr

--- qr_creator.py
+++ qr_creator.py
@@ -1,1 +1,21 @@
+#Author HolyofMouse -- generate qr-code in image
+
 import segno
+import pathlib
+
+path_to_dir='~/Pictures/QR'
+
+
+class Qr_code():
+
+    
+
+    def create_word_in_qr(self, text):
+        self.text = text
+        name_file = input("Write name file and extension(jpg, png, pdf, svg, eps): ")
+        text = segno.make_qr(input())
+        full_file_path=pathlib.Path(path_to_dir).expanduser() / name_file
+        text.save(str(full_file_path), scale=5)
+
+qrcode = Qr_code()
+qrcode.create_word_in_qr('hello world')
